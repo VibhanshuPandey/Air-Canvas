97b38531f882be9ee3d6be2f3e352c0275c0e3ed
VibhanshuPandey <111169133+VibhanshuPandey@users.noreply.github.com>
2024-06-16 00:08:13 +0530

    Update Air-canvas.py

--- Air-canvas.py
+++ Air-canvas.py
@@ -7,7 +7,6 @@ def setValues(x):
    print("")
 
 
-
 # Creating the trackbars needed for adjusting the marker colour
 cv2.namedWindow("Color detectors")
 cv2.createTrackbar("Upper Hue", "Color detectors", 153, 180,setValues)
